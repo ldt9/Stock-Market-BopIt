f0047f774689a509b6c752743d10ebbcdfc3e07f
Lucas Troy <84938803+ldt9@users.noreply.github.com>
2023-02-28 11:16:23 -0500

    Updated Gyroscope for initialization

--- bopIt.ino
+++ bopIt.ino
@@ -1,9 +1,6 @@
 // https://mhsp18object.wordpress.com/2018/05/06/project-2-diy-arduino-sensor-bop-it/
 #include <string.h>
 
-//Initialize the Gryroscope
-#include "GY521.h" // Gyroscope Library
-
 //Create an Instance of the Gryroscope
 #include <Arduino_LSM6DSOX.h>
 
@@ -25,11 +22,8 @@ LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change acc
 //sensor pins
 int buyItPin = 7;
 int sellItPin = 8;
-int holdItPin = 9; //Use gyroscope pitch and yaw
-
-//"start button" pin
-// int rfidScannerPin = 5;
-// int rfidScannerState = 0;
+int holdItPin = 6; //Use gyroscope pitch and yaw
+int bypass = 9; //To bypass the RFID Scanner during testing
 
 //action to perform during each step
 long action = 0;
@@ -48,34 +42,30 @@ int keepPlaying = 0;
 int actionCompleted = 0;
 int score = 0;
 int finalScore = 0;
+int bypassTrue = 0;
 
 
 void setup() {
 	//initialize software serial monitor and give a delay for lcd to load
 	lcd.begin();
-  lcd.backlight();
+  	lcd.backlight();
 	delay(500);
 
-  //setup for the RFID Scanner
-  Serial.begin(9600);   // Initialize comms with the PC
-  while (!Serial);      // Do nothing until Serial comms are established
-  SPI.begin();          //Initialize SPI Bus
-  mfrc522.PCD_Init();   //Initialize the RFID Scanner
-  delay(4);             //Delay for the scanner, can be increased depending on the board
+  	//Initialize the gyroscope
+  	IMU.begin();
+
+  	//setup for the RFID Scanner
+ 	Serial.begin(9600);   // Initialize comms with the PC
+  	while (!Serial);      // Do nothing until Serial comms are established
+  	SPI.begin();          //Initialize SPI Bus
+  	mfrc522.PCD_Init();   //Initialize the RFID Scanner
+  	delay(4);             //Delay for the scanner, can be increased depending on the board
 
 	//setting pin modes
 	// pinMode(rfidScannerPin, INPUT);
 	pinMode(buyItPin, INPUT);       //Button controls this pin
 	pinMode(sellItPin, INPUT);      //Sound detector controls this pin
-
-	// set start button state to LOW
-	// rfidScannerState = 0;
-
-	// // display game start code on lcd
-  // lcd.print("     Scan"); 
-  // lcd.setCursor(3, 1);
-  // lcd.print("to Start!");
-  // delay(500);
+  	pinMode(bypass, INPUT);         //Button to bypass RFID Scanner to start game
 
 	char scoreString[10];
 }
@@ -83,9 +73,9 @@ void setup() {
 void actionDetector(int action, int actionPin, String actionText, bool digital) {
 	//if action selected is one, then tell user to "Buy It"
 	// lcd.clear();
-  // lcd.print(actionText);
+  	// lcd.print(actionText);
 
-  float x, y, z;
+  	float x, y, z;
 
 	//Read button value to track if user has changed it once prompted
 	if (digital) {
@@ -93,15 +83,15 @@ void actionDetector(int action, int actionPin, String actionText, bool digital) 
 	}
 	else {
 		// state1 = analogRead(sensor); //Adjust this for the gyroscope
-    IMU.readAcceleration(x, y, z);
-    // lcd.clear();
-    state1 = int(x) + int(y) + int(z);
-    // lcd.print(roll);
-    // lcd.clear();
+    	IMU.readAcceleration(x, y, z);
+    	// lcd.clear();
+    	state1 = int(x) + int(y) + int(z);
+    	// lcd.print(roll);
+    	// lcd.clear();
   }
 
 	//write "Buy It" to lcd to prompt user
-  lcd.clear();
+  	lcd.clear();
 	lcd.print(actionText);
 
 	//record the time you prompted the user
@@ -116,11 +106,11 @@ void actionDetector(int action, int actionPin, String actionText, bool digital) 
 		}
 		else {
 			// state2 = analogRead(sensor);
-      IMU.readAcceleration(x, y, z);
-      // lcd.clear();
-      state2 = int(x) + int(y) + int(z);
-      // lcd.print(yaw);
-      // lcd.clear();
+		      	IMU.readAcceleration(x, y, z);
+		      	// lcd.clear();
+		      	state2 = int(x) + int(y) + int(z);
+		      	// lcd.print(yaw);
+		      	// lcd.clear();
 		}
 		//if the switch state changed, then the user completed action
 		if (state2 != state1){
@@ -136,21 +126,21 @@ void actionDetector(int action, int actionPin, String actionText, bool digital) 
 	//once while loop stops (either due to time running out or completing action)
 	//check to see if action was comleted and adjust settings accordingly
 	if (actionCompleted == 1){
-    lcd.clear();
+    		lcd.clear();
 		lcd.print("action completed");
-    digitalWrite(10, HIGH); // Green LED
+    		digitalWrite(10, HIGH); // Green LED
 		// tone(3, 10000, 250);
 		delay(250);
 		// noTone(3);
 	}
 	else if (actionCompleted == 0){
-    lcd.clear();
+    		lcd.clear();
 		lcd.print("action failed");
 		// tone(3, 500, 750);
 		delay(750);
 		// noTone(3);
 		finalScore = score;
-    score = 0; //reset score to 0 for new game
+    		score = 0; //reset score to 0 for new game
 		char scoreString[10];
 		sprintf(scoreString,"%4d",finalScore);
 		//scoreString = String(finalScore);
@@ -164,15 +154,15 @@ void actionDetector(int action, int actionPin, String actionText, bool digital) 
 		// lcd.write("                ");
 		// lcd.write(254);
 		// lcd.write(128);
-    lcd.clear();
+    		lcd.clear();
 		lcd.print("GAME OVER!");
 		// lcd.write(254);
 		// lcd.write(192);
-    lcd.clear();
+    		lcd.clear();
 		lcd.write("Score");
 		// lcd.write(254);
 		// lcd.write(199);
-    lcd.clear();
+    		lcd.clear();
 		lcd.print(scoreString);
 
 		delay(2000);                        
@@ -201,15 +191,16 @@ void loop() {
 	// lcd.write("Press Start");
 
 	// display game start code on lcd
-  lcd.clear();
-  lcd.print("     Scan"); 
-  lcd.setCursor(3, 1);
-  lcd.print("to Start!");
-  delay(500);
+  	lcd.clear();
+  	lcd.print("     Scan"); 
+  	lcd.setCursor(3, 1);
+  	lcd.print("to Start!");
+  	delay(500);
 
 	//If card is scanned, go into game.
 	// if (! mfrc522.PICC_ReadCardSerial()){
-	if (mfrc522.PICC_IsNewCardPresent()){
+  	bypassTrue = digitalRead(bypass);
+	if (mfrc522.PICC_IsNewCardPresent() | bypassTrue){
 
 	//variable to control the while loop running the game until user has failed
 	keepPlaying = 1;
@@ -219,14 +210,14 @@ void loop() {
 			//pick an action to assign the user via a random number generator – upper bound is exclusive
 			action = random(1,4);
 			actionCompleted = 0;
-      lcd.clear();
+      			lcd.clear();
 			lcd.print(action);
-      lcd.clear();
+      			lcd.clear();
 			// Serial.print(action);
 			// Serial.println(" ");
 		  
 			//if else statements to prompt user and check action
-		    switch(action) {
+		    	switch(action) {
 				case 1:
 					actionDetector(action, buyItPin, "Buy It!", 1);
 					break;
